d00ba44e8869ef60e4611411cddcd36f66822eb6
Sheriff Aderombi <ayaderombi@gmail.com>
2024-10-08 22:23:04 +0100

    travel journal

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import Navbar from "./components/Navbar";
+import Card from "./components/Card";
+import Footer from "./components/Footer";
+import data from "./data";
+
+export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.key} {...item} />;
+  });
+
+  return (
+    <div>
+      <Navbar />
+      <section className="cards-list">{cards}</section>
+      <Footer />
+    </div>
+  )
+}
+
+
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export default function Footer() {
+    return (
+      <footer className="footer">
+        <small>
+          <a
+            className="footer--link"
+            href="https://emanueledelmonte.it"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Sheriff Aderombi - 2024
+          </a>
+        </small>
+      </footer>
+    );
+  }
